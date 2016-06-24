461fa59d1bb1a8b82e89eb79825807b40026f777
Magda Mukhtar <magda.mukhtar@bloomreach.com>
2016-06-23 20:14:45 -0700

    Set up angular routing and templates. Include minimal template for home page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,21 @@
-tldr = angular.module('tldr', []);
+tldr = angular.module('tldr', [
+  'templates',
+  'ngRoute',
+  'controllers',
+]);
+
+tldr.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/',
+        {
+          templateUrl: "index.html",
+          controller: 'SummaryController'
+        });
+  }
+]);
+
+controllers = angular.module('controllers', []);
+controllers.controller("SummaryController", [ '$scope',
+  function($scope) {}
+]);
